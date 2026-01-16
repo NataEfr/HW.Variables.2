63d803bd1c366b6a4a9c87cb1396e8eba53ae617
Nataliia <nataliefremova88@yandex.ru>
2026-01-16 17:25:24 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+ //Задача 1
+
+ int iNt = 10;
+ byte bYTe = 2;
+ short sHort = 328;
+ long lONg = 2000000;
+ float fLOat = 2.56f;
+ double dOUBle = 3.785963d;
+
+ System.out.println("Значение переменной iNt с типом int равно: " +iNt);
+ System.out.println("Значение переменной bYTe с типом byte равно: " +bYTe);
+ System.out.println("Значение переменной sHort с типом short равно: " +sHort);
+ System.out.println("Значение переменной lONg с типом long равно: " +lONg);
+ System.out.println("Значение переменной fLOat  с типом float равно: " +fLOat);
+ System.out.println("Значение переменной dOUBle с типом double равно: " +dOUBle);
 
 
+ //Задача 2
     }
     }
