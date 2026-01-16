dde7a0457619ac17b20461643499be4d98e87515
Nataliia <nataliefremova88@yandex.ru>
2026-01-16 17:38:28 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,25 @@ public class Main {
 
 
  //Задача 2
+
+   float f2 = 27.12f;
+   long l2 = 987678965549L;
+   float f3 = 2.786F;
+   int i2 = 569;
+   int i3 = -159;
+   short s2 = 27897;
+   byte b2 = 67;
+
+
+//Задача 3
+
+    byte ludPav = 23;
+    byte annSerg = 27;
+    byte ekatAndr = 30;
+    int pepper = 480;
+    int students = ludPav+annSerg+ekatAndr;
+    int everySudPaper = pepper/students;
+System.out.println("На каждого ученика рассчитано "+everySudPaper+" листов бумаги.");
+
     }
     }
